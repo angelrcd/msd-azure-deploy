88c4376857776142ae7df5bd83e64de6833d6ff0
Angel Romero <mrangelrcd@gmail.com>
2023-08-24 21:05:45 +0200

    Can extract data from deployment output.

--- deploy.sh
+++ deploy.sh
@@ -11,3 +11,9 @@ az deployment group create \
   --template-file ./template.json \
   --parameters ./parameters.json \
   --parameters repositoryToken=$GITHUB_TOKEN
+
+# Get the last deployment ouput and store it in a variable
+DEPLOYMENT_OUTPUT=$(az deployment group show -g mysweetdreamsrg -n devenvironment --query 'properties.outputs')
+
+# Separate many output values in separate variables
+FRONTEND_URI=$(echo "$DEPLOYMENT_OUTPUT" | jq -r '.frontendUri' | jq -r '.value')
