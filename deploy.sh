874f1b035e50e60bc9e7aa3314dec360de792a0a
Angel Romero <mrangelrcd@gmail.com>
2023-08-26 11:07:33 +0200

    Add mongo cosmos db account

--- deploy.sh
+++ deploy.sh
@@ -18,7 +18,8 @@ DEPLOYMENT_OUTPUT=$(az deployment group show -g mysweetdreamsrg -n devenvironmen
 # Separate many output values in separate variables
 FRONTEND_URI=$(echo "$DEPLOYMENT_OUTPUT" | jq -r '.frontendUri.value')
 STORAGE_ACCOUNT_KEY=$(echo "$DEPLOYMENT_OUTPUT" | jq -r '.storageAccountKey.value')
+COSMOS_DB_CONNECTION_STRING=$(az cosmosdb keys list --type connection-strings --name msdmongodbarc --resource-group mysweetdreamsrg | jq -r '.connectionStrings[] | select(.keyKind == "Primary").connectionString')
 
 # Create container in storage account and upload default user profile pic
-az storage container create --name user-pfp --account-name $STORAGE_ACCOUNT_NAME --public-access blob
-az storage blob upload --account-name $STORAGE_ACCOUNT_NAME --container-name user-pfp --file default.png
+az storage container create --name user-pfp --account-name $STORAGE_ACCOUNT_NAME --public-access blob --account-key $STORAGE_ACCOUNT_KEY
+az storage blob upload --account-name $STORAGE_ACCOUNT_NAME --container-name user-pfp --file default.png --account-key $STORAGE_ACCOUNT_KEY
