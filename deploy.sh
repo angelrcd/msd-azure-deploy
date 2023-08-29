6db3f5c9f1427ec92906322de97ae61dc2cf2c18
Angel Romero <mrangelrcd@gmail.com>
2023-08-29 10:56:01 +0200

    Add App service web app for backend with app configuration

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,8 @@ az deployment group create \
   --resource-group mysweetdreamsrg \
   --template-file ./template.json \
   --parameters ./parameters.json \
-  --parameters repositoryToken=$GITHUB_TOKEN
+  --parameters repositoryToken=$GITHUB_TOKEN \
+  --parameters dockerRegistryPassword='dckr_pat_dVGfw1Qx5YN3Get1SqgeSyrnR6M'
 
 # Get the last deployment ouput and store it in a variable
 DEPLOYMENT_OUTPUT=$(az deployment group show -g mysweetdreamsrg -n devenvironment --query 'properties.outputs')
@@ -22,4 +23,16 @@ COSMOS_DB_CONNECTION_STRING=$(az cosmosdb keys list --type connection-strings --
 
 # Create container in storage account and upload default user profile pic
 az storage container create --name user-pfp --account-name $STORAGE_ACCOUNT_NAME --public-access blob --account-key $STORAGE_ACCOUNT_KEY
-az storage blob upload --account-name $STORAGE_ACCOUNT_NAME --container-name user-pfp --file default.png --account-key $STORAGE_ACCOUNT_KEY
+az storage blob upload --account-name $STORAGE_ACCOUNT_NAME --container-name user-pfp --file default.png --account-key $STORAGE_ACCOUNT_KEY --overwrite
+
+# Set App configuration of the backend App Service Web App
+az webapp config appsettings set -n msd-api-arc -g mysweetdreamsrg --settings \
+PORT=$PORT \
+NODE_ENV=$NODE_ENV \
+SALT_SEED=$SALT_SEED \
+SECRET_OR_PRIVATE_KEY=$SECRET_OR_PRIVATE_KEY \
+ALLOW_ORIGIN="https://${FRONTEND_URI}" \
+STORAGE_CONNECTION_STRING="DefaultEndpointsProtocol=https;AccountName=${STORAGE_ACCOUNT_NAME};AccountKey=${STORAGE_ACCOUNT_KEY};EndpointSuffix=core.windows.net" \
+STORAGE_CONTAINER_NAME="user-pfp" \
+MONGO_IP=$COSMOS_DB_CONNECTION_STRING
+
